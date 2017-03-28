8514532872ff7edb0484bb759318e7b354720f64
abxcytf <lawrencelou88@gmail.com>
2017-03-28 18:54:50 -0400

    Create ArrayQ4.LeetCode15.java

---
+++ Week01/ArrayQ4.LeetCode15.java
@@ -0,0 +1,40 @@
+/*
+15. 3Sum
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? 
+Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) return result;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] == 0) {
+                    List<Integer> answer = new ArrayList<>(3);
+                    answer.add(nums[i]);
+                    answer.add(nums[left++]);
+                    answer.add(nums[right--]);
+                    result.add(answer);
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (nums[i] + nums[left] + nums[right] < 0) 
+                    left++;
+                else right--;
+            }
+        }
+        return result;
+    }
+}
