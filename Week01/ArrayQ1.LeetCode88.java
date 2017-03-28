3ebd2a5c1e34eb768714321e2f80b6ff78691434
abxcytf <lawrencelou88@gmail.com>
2017-03-28 18:53:08 -0400

    Create ArrayQ1.LeetCode88.java

---
+++ Week01/ArrayQ1.LeetCode88.java
@@ -0,0 +1,28 @@
+/*
+88. Merge Sorted Array
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. 
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+*/
+
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1;
+        int index = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[index--] = nums1[i--];
+            } else {
+                nums1[index--] = nums2[j--];
+            }
+        }
+        while (i >= 0) {
+            nums1[index--] = nums1[i--];
+        }
+        while (j >= 0) {
+            nums1[index--] = nums2[j--];
+        }
+    }
+}
