b09920e95faf492ba1e9b637f67e75624db1b23b
abxcytf <lawrencelou88@gmail.com>
2017-03-30 14:23:55 -0400

    Update LeetCodeQ206.Reverse_LinkedList.java

--- Week02_Lec/LeetCodeQ206.Reverse_LinkedList.java
+++ Week02_Lec/LeetCodeQ206.Reverse_LinkedList.java
@@ -17,11 +17,33 @@ public class Solution {
         //if (head == null || head.next == null) return head;
         return reverseList_tail_recursion(head, null);
     }
-    
+    // tail-recursion
     private ListNode reverseList_tail_recursion(ListNode head, ListNode newHead) {
         if (head == null) return newHead;
         ListNode temp = head.next;
         head.next = newHead;
         return reverseList_tail_recursion(temp, head);
+    }
+	
+	
+    //non-tail-recursion
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+    
+    //iterative
+    public ListNode reverse(ListNode head) {
+        ListNode prevNode = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = prevNode;
+            prevNode = head;
+            head = nextNode;
+        }
+        return prevNode;
     }
 }
