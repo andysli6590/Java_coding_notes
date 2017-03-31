b93f0209cd6eeec7f4bcdb9239ced8f3e3be597e
abxcytf <lawrencelou88@gmail.com>
2017-03-31 13:22:43 -0400

    Create LeetCodeQ285.Inorder_Successor_In_BST.java

---
+++ Week02_Lec/LeetCodeQ285.Inorder_Successor_In_BST.java
@@ -0,0 +1,27 @@
+/*
+285. Inorder Successor in BST
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+Note: If the given node has no in-order successor in the tree, return null.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode inorderSuccessor_recursion(TreeNode root, TreeNode p) {
+        if (root == null) return null;
+        if (root.val > p.val) {
+            TreeNode left = inorderSuccessor(root.left, p);
+            return left == null ? root : left;
+        } else {
+            return inorderSuccessor(root.right, p);
+        }
+    }
+}
