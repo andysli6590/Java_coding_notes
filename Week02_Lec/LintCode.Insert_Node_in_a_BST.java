7b19582b5a7af3c25ea9326b51354ec656d68aef
abxcytf <lawrencelou88@gmail.com>
2017-04-03 20:10:44 -0400

    Create LintCode.Insert_Node_in_a_BST.java

---
+++ Week02_Lec/LintCode.Insert_Node_in_a_BST.java
@@ -0,0 +1,46 @@
+/*
+Insert Node in a Binary Search Tree 
+Given a binary search tree and a new tree node, insert the node into the tree. 
+You should keep the tree still be a valid binary search tree.
+You can assume there is no duplicate values in this tree + node.
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param node: insert this node into the binary search tree
+     * @return: The root of the new binary search tree.
+     */
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+        if (root == null) return node;
+        TreeNode prev = null;
+        TreeNode current = root;
+        while (current != null) {
+            prev = current;
+            if (current.val < node.val) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+        
+        if (prev.val < node.val) {
+            prev.right = node;
+        } else {
+            prev.left = node;
+        }
+        return root;
+    }
+}
