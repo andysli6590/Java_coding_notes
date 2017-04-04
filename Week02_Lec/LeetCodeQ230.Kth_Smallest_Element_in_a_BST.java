4668476cc6bdd3f5398eb55747f8312b6170313a
abxcytf <lawrencelou88@gmail.com>
2017-04-04 13:42:55 -0400

    Update LeetCodeQ230.Kth_Smallest_Element_in_a_BST.java

--- Week02_Lec/LeetCodeQ230.Kth_Smallest_Element_in_a_BST.java
+++ Week02_Lec/LeetCodeQ230.Kth_Smallest_Element_in_a_BST.java
@@ -35,4 +35,71 @@ public class Solution {
         }
         helper(root.right, answer);
     }
+    
+    //==============================================================================================
+    //if we can modify the tree structure, and add  count attribute
+    public class TreeNodeWithCount {
+        int val;
+        int lCount;
+        TreeNodeWithCount left;
+        TreeNodeWithCount right;
+        TreeNodeWithCount(int val){
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+    
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null) return -1;
+        TreeNodeWithCount rootWithCount = createBSTWithCount(root);
+        return kthSmallestWithCount(rootWithCount, k);
+    }
+    
+    private TreeNodeWithCount createBSTWithCount(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        TreeNodeWithCount rootWithCount = null;
+        while(!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNodeWithCount nodeWithCount = new TreeNodeWithCount(node.val);
+            rootWithCount = insertBSTWithCount(rootWithCount, nodeWithCount);
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.offer(node.right);
+        }
+        return rootWithCount;
+    }
+    
+    private TreeNodeWithCount insertBSTWithCount(TreeNodeWithCount rootWithCount, TreeNodeWithCount nodeWithCount) {
+        TreeNodeWithCount cur = rootWithCount, parent = rootWithCount;
+        while (cur != null) {
+            parent = cur;
+            if (nodeWithCount.val < cur.val) {
+                cur.lCount++;
+                cur = cur.left;
+            } else {
+                cur = cur.right;
+            }
+        }
+        if (rootWithCount == null) {
+            rootWithCount = nodeWithCount;
+        } else if (parent.val < nodeWithCount.val) {
+            parent.right = nodeWithCount;
+        } else {
+            parent.left = nodeWithCount;
+        }
+        return rootWithCount;
+    }
+    
+    private int kthSmallestWithCount(TreeNodeWithCount rootWithCount, int k) {
+        while (rootWithCount != null) {
+            if (k == rootWithCount.lCount + 1) return rootWithCount.val;
+            else if (k <= rootWithCount.lCount) {
+                rootWithCount = rootWithCount.left;
+            } else {
+                k = k - rootWithCount.lCount - 1;
+                rootWithCount = rootWithCount.right;
+            }
+        }
+        return -1;
+    }
 }
