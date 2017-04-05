de64d728167ceb14cdbdbceddd3e7f67e4e70a18
abxcytf <lawrencelou88@gmail.com>
2017-04-05 15:21:41 -0400

    Create LeetCodeQ102.Binary_Tree_Level_Order_Traversal.java

---
+++ Week02/LeetCodeQ102.Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,48 @@
+/*
+102. Binary Tree Level Order Traversal
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offerLast(root);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.pollFirst();
+                level.add(current.val);
+                if (current.left != null) queue.offerLast(current.left);
+                if (current.right != null) queue.offerLast(current.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
