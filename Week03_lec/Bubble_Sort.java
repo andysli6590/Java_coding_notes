bbba72c405b17182a784753b6785b3fe9f9e4078
abxcytf <lawrencelou88@gmail.com>
2017-04-09 19:46:55 -0400

    Update Bubble_Sort.java

--- Week03_lec/Bubble_Sort.java
+++ Week03_lec/Bubble_Sort.java
@@ -3,16 +3,45 @@ public class Solution {
      * @param A an integer array
      * @return void
      */
+    //Time: O(n^2), space: O(1)
     public void sortIntegers(int[] nums) {
         //bubble sort
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length - i; j++) {
+            for (int j = 1; j < nums.length; j++) {
                 if (nums[j - 1] > nums[j]) {
-                    int temp = nums[j - 1];
-                    nums[j - 1] = nums[j];
-                    nums[j] = temp;
+                    swap(nums, j - 1, j);
                 }
             }
         }
     }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        int nums[i] = nums[j];
+        int nums[j] = temp;
+    }
+    
+    /******************************************************************/
+    //optimized bubble sort
+    public void sortIntegers(int[] nums) {
+        //bubble sort
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length - i; j++) { 
+                //the optimized is because each loop 1 number will be settle to his finally location
+                //so we dont need to check every time
+                if (nums[j - 1] > nums[j]) {
+                    swap(nums, j - 1, j);
+                }
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        if (nums[i] != nums[j]) {
+            nums[i] ^= nums[j];
+            nums[j] ^= nums[i];
+            nums[i] ^= nums[j];
+        }
+    }
+    
 }
