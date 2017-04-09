4cf54fb9f5860114b3a3bde7b9e7d27bcf550b8f
abxcytf <lawrencelou88@gmail.com>
2017-04-09 19:53:41 -0400

    Update Selection_Sort.java

--- Week03_lec/Selection_Sort.java
+++ Week03_lec/Selection_Sort.java
@@ -3,24 +3,32 @@ public class Solution {
      * @param A an integer array
      * @return void
      */
-    public void sortIntegers(int[] A) {
+    public void sortIntegers(int[] nums) {
         //selection sort
-        if (A == null || A.length <= 1) return;
-        int n = A.length;
+        if (nums == null || nums.length <= 1) return;
+        int n = nums.length;
         int minValueIndex = 0;
         
         for (int i = 0; i < n; i++) {
             minValueIndex = i;
             
             for (int j = i+1; j < n; j ++) {
-                if (A[j] < A[minValueIndex]) minValueIndex = j;
+                if (nums[j] < nums[minValueIndex]) minValueIndex = j;
             }
             
-            //swap A[j] and A[minValueIndex]
-            int temp = A[i];
-            A[i] = A[minValueIndex];
-            A[minValueIndex] = temp;
+//             //swap A[j] and A[minValueIndex]
+//             int temp = A[i];
+//             A[i] = A[minValueIndex];
+//             A[minValueIndex] = temp;
+            swap(nums, j, minValueIndex);
         }
-        
+    }
+    
+    private void swap(nums, int i, int j) {
+        if (nums[i] != nums[j]) {
+            nums[i] ^= nums[j];
+            nums[j] ^= nums[i];
+            nums[i] ^= nums[j];
+        }
     }
 }
