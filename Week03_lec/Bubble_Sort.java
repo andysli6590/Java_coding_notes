dfc77c2de73549d0be1337b7991194287222ee77
abxcytf <lawrencelou88@gmail.com>
2017-04-09 19:55:48 -0400

    Update Bubble_Sort.java

--- Week03_lec/Bubble_Sort.java
+++ Week03_lec/Bubble_Sort.java
@@ -3,7 +3,7 @@ public class Solution {
      * @param A an integer array
      * @return void
      */
-    //Time: O(n^2), space: O(1)
+    //Time: O(n^2), space: O(1), unstable
     public void sortIntegers(int[] nums) {
         //bubble sort
         for (int i = 0; i < nums.length; i++) {
