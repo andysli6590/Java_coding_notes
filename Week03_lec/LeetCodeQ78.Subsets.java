38478f3d471a418e70ef6b44a8823187f3fd4f1b
abxcytf <lawrencelou88@gmail.com>
2017-04-10 15:10:11 -0400

    Update LeetCodeQ78.Subsets.java

--- Week03_lec/LeetCodeQ78.Subsets.java
+++ Week03_lec/LeetCodeQ78.Subsets.java
@@ -18,3 +18,71 @@ If nums = [1,2,3], a solution is:
   []
 ]
 */
+
+
+public class Solution {
+    
+    //DFS, back tracking
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) return result;
+        if (nums.length == 0) {
+            result.add(new ArrayList<Integer>());
+            return result;
+        }
+        helper(nums, 0, result, new ArrayList<>());
+        return result;
+    }
+    
+    private void helper(int[] nums, int index, List<List<Integer>> result, List<Integer> path) {
+        result.add(new ArrayList<>(path));
+        for (int i = index; i < nums.length; i++) {
+            path.add(nums[i]);
+            helper(nums, i + 1, result, path);
+            path.remove(path.size() - 1);
+        }
+    }
+  
+    /*********************************************************************/
+    //iteration, normal implementation
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) return result;
+        result.add(new ArrayList<Integer>());
+        //Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int prevNum = result.size();
+            for (int j = 0; j < prevNum; j++) {
+                List<Integer> subset = new ArrayList<>(result.get(j));
+                subset.add(nums[i]);
+                result.add(new ArrayList<>(subset));
+            }
+        }
+        return result;
+    }
+  
+    /*
+    // 1 << n is 2^n
+    // each subset equals to an binary integer between 0 .. 2^n - 1
+    // 0 -> 000 -> []
+    // 1 -> 001 -> [1]
+    // 2 -> 010 -> [2]
+    // ..
+    // 7 -> 111 -> [1,2,3]
+    */
+    public List<List<Integer>> subsets(int[] nums) {
+          List<List<Integer>> result = new ArrayList<>();
+          int n = nums.length;
+          Arrays.sort(nums);
+          for (int i = 0; i < (1 << n); i++) {
+              List<Integer> subset = new ArrayList<>();
+              for (int j = 0; j < n; j++) {
+                  if ((i & (1 << j)) != 0) { // check whether the jth digit in i's binary representation is 1
+                      subset.add(nums[j]);
+                  }
+              }
+              result.add(subset);
+          }
+          return result;
+      }
+}
