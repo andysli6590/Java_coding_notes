126abb2ac83bde2fdd00757b7cff48152a5484db
abxcytf <lawrencelou88@gmail.com>
2017-04-10 01:27:15 -0400

    Update Insertion_sort.java

--- Week03_lec/Insertion_sort.java
+++ Week03_lec/Insertion_sort.java
@@ -1,3 +1,44 @@
 /*
 两种insertion的写法，一种是用swap是unstable的，一种是整体在移动位置的方法是stable的。
 */
+
+
+public class Solution {
+    /**
+     * @param A an integer array
+     * @return void
+     */
+    public void sortIntegers(int[] nums) {
+        //unstable implementation
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (nums[j-1] > nums[j]) {
+                    swap(nums, j - 1, j);
+                }
+            }
+        }
+    }
+  
+    private void swap(int[] nums, int i, int j) {
+      if (nums[i] != nums[j]) {
+          nums[i] ^= nums[j];
+          nums[j] ^= nums[i];
+          nums[i] ^= nums[j];
+      }
+    }
+  
+  /**********************************/
+  //stable implementation
+  public void sortIntegers(int[] A) {
+        int x, j;
+        for (int i = 1; i < A.length; i++) {
+            x = A[i];
+            j = i - 1;
+            while (j >= 0 && A[j] > x) {
+                A[j+1] = A[j];
+                j--;
+            }
+            A[j+1] = x;
+        }
+    }
+}
