03ef029ac035993de0ad342ee86426447774c5f5
abxcytf <lawrencelou88@gmail.com>
2017-04-10 18:29:36 -0400

    Update LeetCodeQ103.Binary_Tree_Zigzag_Level_Order_Traversal.java

--- Week03/LeetCodeQ103.Binary_Tree_Zigzag_Level_Order_Traversal.java
+++ Week03/LeetCodeQ103.Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -17,3 +17,39 @@ return its zigzag level order traversal as:
   [15,7]
 ]
 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        boolean flag = true;
+        queue.offerLast(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            TreeNode current = null;
+            List<Integer> level = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                current = queue.pollFirst();
+                if (flag) {
+                    level.add(current.val);
+                } else {
+                    level.add(0, current.val);
+                }
+                if (current.left != null) queue.offerLast(current.left);
+                if (current.right != null) queue.offerLast(current.right);
+            }
+            result.add(level);
+            flag = !flag;
+        }
+        return result;
+    }
+}
