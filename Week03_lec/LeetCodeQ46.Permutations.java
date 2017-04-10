1fd11ce70287cef0215e96642b83ffab33c7f80e
abxcytf <lawrencelou88@gmail.com>
2017-04-10 14:40:30 -0400

    Update LeetCodeQ46.Permutations.java

--- Week03_lec/LeetCodeQ46.Permutations.java
+++ Week03_lec/LeetCodeQ46.Permutations.java
@@ -14,23 +14,51 @@ For example,
 ]
 */
 public class Solution {
-    
-    //iteration
-    public List<List<Integer>> permute(int[] nums) {
+  //iteration
+  public List<List<Integer>> permute(int[] nums) {
+      List<List<Integer>> result = new ArrayList<>();
+      if (nums == null) return result;
+      result.add(new ArrayList<Integer>());
+      for (int i = 0; i < nums.length; i++) {
+          List<List<Integer>> newResult = new ArrayList<>();
+          for(int j = 0; j <= i; j++) {
+              for (List<Integer> list : result) {
+                  List<Integer> newList = new ArrayList<>(list);
+                  newList.add(j, nums[i]);
+                  newResult.add(newList);
+              }
+          }
+          result = newResult;
+      }
+      return result;
+  }
+
+  /************************************/
+  //DFS, back tracking
+  public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null) return result;
-        result.add(new ArrayList<Integer>());
-        for (int i = 0; i < nums.length; i++) {
-            List<List<Integer>> newResult = new ArrayList<>();
-            for(int j = 0; j <= i; j++) {
-                for (List<Integer> list : result) {
-                    List<Integer> newList = new ArrayList<>(list);
-                    newList.add(j, nums[i]);
-                    newResult.add(newList);
-                }
-            }
-            result = newResult;
+        if (nums.length == 0) {
+            result.add(new ArrayList<Integer>());
+            return result;
         }
+        boolean[] visited = new boolean[nums.length];
+        helper(nums, visited, result, new ArrayList<Integer>());
         return result;
+    }
+
+    private void helper(int[] nums, boolean[] visited, List<List<Integer>> result, List<Integer> answer) {
+        if (answer.size() == nums.length) {
+            result.add(new ArrayList<>(answer));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) continue;
+            answer.add(nums[i]);
+            visited[i] = true;
+            helper(nums, visited, result, answer);
+            answer.remove(answer.size() - 1);
+            visited[i] = false;
+        }
     }
 }
