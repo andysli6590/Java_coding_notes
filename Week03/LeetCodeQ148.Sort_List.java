4f109100b22544784f62db651be54c5e2a3ce849
abxcytf <lawrencelou88@gmail.com>
2017-04-10 23:05:55 -0400

    Update LeetCodeQ148.Sort_List.java

--- Week03/LeetCodeQ148.Sort_List.java
+++ Week03/LeetCodeQ148.Sort_List.java
@@ -53,4 +53,64 @@ public class Solution {
         }
         return dummy.next;
     }
+    
+    /***********************************************************/
+    //Quick Sort: time complexity: O(nlogn), space complexity: O(1) + O(logn) system stack frame
+    //先整体有序，在局部有序
+        public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode mid = findMiddle(head); 
+        ListNode leftDummy = new ListNode(-1);
+        ListNode leftTail = leftDummy;
+        ListNode rightDummy = new ListNode(-1);
+        ListNode rightTail = rightDummy;
+        ListNode middleDummy = new ListNode(-1);
+        ListNode middleTail = middleDummy;
+        while (head != null) {
+            if (head.val < mid.val) {
+                leftTail.next = head;
+                leftTail = head;
+            } else if (head.val > mid.val) {
+                rightTail.next = head;
+                rightTail = head;
+            } else {
+                middleTail.next = head;
+                middleTail = head;
+            }
+            head = head.next;
+        }
+        leftTail.next = null;
+        rightTail.next = null;
+        middleTail.next = null;
+        ListNode left = sortList(leftDummy.next);
+        ListNode right = sortList(rightDummy.next);
+        return concat(left, middleDummy.next, right);
+    }
+    
+    private ListNode findMiddle(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+    private ListNode concat(ListNode left, ListNode middle, ListNode right) {
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+        tail.next = left; tail = getTail(tail);
+        tail.next = middle; tail = getTail(tail);
+        tail.next = right;
+        return dummy.next;
+    }
+    
+    private ListNode getTail(ListNode head) {
+        if (head == null || head.next == null) return head;
+        while (head.next != null) {
+            head = head.next;
+        }
+        return head;
+    }
 }
