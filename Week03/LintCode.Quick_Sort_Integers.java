225fdc38dfecfe8a92d6e4da4761260ed705bd82
abxcytf <lawrencelou88@gmail.com>
2017-04-11 15:59:22 -0400

    Create LintCode.Quick_Sort_Integers.java

---
+++ Week03/LintCode.Quick_Sort_Integers.java
@@ -0,0 +1,5 @@
+/*
+Sort Integers II 
+Given an integer array, sort it in ascending order. Use quick sort, merge sort, heap sort or any O(nlogn) algorithm.
+Given [3, 2, 1, 4, 5], return [1, 2, 3, 4, 5].
+*/
