af3fd43e951d904f0fcc5aec26fb52ab4f7df349
abxcytf <lawrencelou88@gmail.com>
2017-04-18 15:49:35 -0400

    Update LeetCodeQ40.Combination_Sum_II.java

--- Week04_Lec/LeetCodeQ40.Combination_Sum_II.java
+++ Week04_Lec/LeetCodeQ40.Combination_Sum_II.java
@@ -1,6 +1,7 @@
 /*
 40. Combination Sum II
-Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+Given a collection of candidate numbers (C) and a target number (T), 
+find all unique combinations in C where the candidate numbers sums to T.
 
 Each number in C may only be used once in the combination.
 
