b04045fba556e2c3132781d0c58add10cd977749
abxcytf <lawrencelou88@gmail.com>
2017-04-19 21:49:42 -0400

    Create LeetCodeQ377.Combination_Sum_IV_follow_up.java

---
+++ Week04_Lec/LeetCodeQ377.Combination_Sum_IV_follow_up.java
@@ -0,0 +1,12 @@
+/*
+377.Combination Sum IV follow up
+Follow up:
+What if negative numbers are allowed in the given array?
+How does it change the problem?
+What limitation we need to add to the question to allow negative numbers?
+*/
+
+/*
+In order to handle negative integers in the input, the length of the combination sum needs to be restricted, or the search will not stop. 
+We need to add the allowed max length of the output into the signature.
+*/
