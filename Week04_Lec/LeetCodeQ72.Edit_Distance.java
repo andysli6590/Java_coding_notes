175493725c4fbc48a38c95b375558351eb6858f2
abxcytf <lawrencelou88@gmail.com>
2017-04-20 19:41:06 -0400

    Update LeetCodeQ72.Edit_Distance.java

--- Week04_Lec/LeetCodeQ72.Edit_Distance.java
+++ Week04_Lec/LeetCodeQ72.Edit_Distance.java
@@ -12,7 +12,7 @@ c) Replace a character
 
 public class Solution {
     
-    //memorized search
+    //memorized search, which is speculated from drawing the recursion/solution tree
     public int minDistance(String word1, String word2) {
         int[][] count = new int[word1.length() + 1][word2.length() + 1]; //need to open the array count "" as the first
         return helper(word1, 0, word2, 0, count);
@@ -38,5 +38,32 @@ public class Solution {
         }
         count[i][j] = result;
         return result;
+    }
+    
+    /********************************************************************************/
+    //DP implementation, the introduction rule is speculated from the memorized search
+    public int minDistance(String word1, String word2) {
+        if (word1 == null || word2 == null) return 0;
+        if (word1.length() == 0 || word2.length() == 0) {
+            return word1.length() == 0 ? word2.length() : word1.length();
+        }
+        int[][] match = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            match[i][0] = i;
+        }
+        for (int j = 0; j <= word2.length(); j++) {
+            match[0][j] = j;
+        }
+        
+        for (int i = 0; i < word1.length(); i++) {
+            for (int j = 0; j < word2.length(); j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    match[i + 1][j + 1] = match[i][j];
+                } else {
+                    match[i + 1][j + 1] = Math.min(match[i][j], Math.min(match[i + 1][j], match[i][j + 1])) + 1;
+                }
+            }
+        }
+        return match[word1.length()][word2.length()];
     }
 }
