08b8da7802a652d5a20717d1e3c753347ee87c38
abxcytf <lawrencelou88@gmail.com>
2017-04-19 23:31:44 -0400

    Update LeetCodeQ377.Combination_Sum_IV_follow_up.java

--- Week04_Lec/LeetCodeQ377.Combination_Sum_IV_follow_up.java
+++ Week04_Lec/LeetCodeQ377.Combination_Sum_IV_follow_up.java
@@ -7,7 +7,8 @@ What limitation we need to add to the question to allow negative numbers?
 */
 
 /*
-In order to handle negative integers in the input, the length of the combination sum needs to be restricted, or the search will not stop. 
+In order to handle negative integers in the input, the length of the combination sum needs to be restricted, 
+or the search will not stop. 
 We need to add the allowed max length of the output into the signature.
 */
 
