b3c974e348745cb32107ed3293c32bc3dbd74283
abxcytf <lawrencelou88@gmail.com>
2017-04-21 14:48:04 -0400

    Update LeetCodeQ322.Coin_Change.java

--- Week04_Lec/LeetCodeQ322.Coin_Change.java
+++ Week04_Lec/LeetCodeQ322.Coin_Change.java
@@ -49,4 +49,32 @@ public class Solution {
         count[remain] = (res == Integer.MAX_VALUE ? -1 : res);
         return count[remain];
     }
+    
+    /*****************************************************************************/
+    //dp implementation, the idea is to fill the dp table iteratively according to the memorized search
+    // and get the introduction rule
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0) {
+            return -1;
+        }
+        if (amount <= 0) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0; //initialization初始值
+        Arrays.sort(coins);
+        
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin > i) {
+                    break;
+                }
+                if (dp[i - coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1); //introduction rule
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
 }
