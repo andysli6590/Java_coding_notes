be9c79c1aaf586407fa4c53ecd29990d249f9f51
abxcytf <lawrencelou88@gmail.com>
2017-04-23 02:23:37 -0400

    Update LeetCodeQ64.Minimum_Path_Sum.java

--- Week04/LeetCodeQ64.Minimum_Path_Sum.java
+++ Week04/LeetCodeQ64.Minimum_Path_Sum.java
@@ -39,5 +39,35 @@ public class Solution {
     }
     
     /******************************************************************************/
-    
+    //dp solution, standard induction rule to fill dp table iteratively
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        //定义数组
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        
+        //初始化
+        dp[0][0] = grid[0][0];
+        //initialization left side
+        for (int j = 1; j < m; j++) {
+            dp[j][0] = grid[j][0] + dp[j - 1][0];
+        }
+        
+        //initialization top side
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        
+        //fill up the dp table iteratively
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                //induction rule
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 }
