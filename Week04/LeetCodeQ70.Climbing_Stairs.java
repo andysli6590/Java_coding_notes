a47d0cd5904dd18580a97d69408750c962e919ce
abxcytf <lawrencelou88@gmail.com>
2017-04-24 15:51:18 -0400

    Update LeetCodeQ70.Climbing_Stairs.java

--- Week04/LeetCodeQ70.Climbing_Stairs.java
+++ Week04/LeetCodeQ70.Climbing_Stairs.java
@@ -68,4 +68,49 @@ public class Solution {
         }
         return dp[n];
     }
+    
+    /*********************************************************************************/
+    //rolling array优化，节省空间
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[3];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i % 3] = dp[(i - 1) % 3] + dp[(i - 2) % 3];
+        }
+        return dp[n % 3];
+    }
+    
+    /**********************************************************************************/
+    //两个指针版本的roll array dp
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int current = 0;
+        int prevOneStep = 2;  //i - 1;
+        int prevTwoStep = 1;  //i - 2;
+        for (int i = 3; i <= n; i++) {
+            current =  prevOneStep + prevTwoStep;
+            prevTwoStep = prevOneStep; //update i - 2;
+            prevOneStep = current; //update i - 1
+        }
+        return current;
+    }
 } 
