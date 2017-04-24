a3bbfef1f6b4a1c7e22ff5ca85ea847d212ac33d
abxcytf <lawrencelou88@gmail.com>
2017-04-24 14:42:35 -0400

    Update LeetCodeQ70.Climbing_Stairs.java

--- Week04/LeetCodeQ70.Climbing_Stairs.java
+++ Week04/LeetCodeQ70.Climbing_Stairs.java
@@ -46,4 +46,26 @@ public class Solution {
         count[n] = helper(n - 1, count) + helper(n - 2, count);
         return count[n];
     }
+    
+    /*******************************************************************************/
+    //标准的dp array 解法，iteratively fill dp array
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
 } 
