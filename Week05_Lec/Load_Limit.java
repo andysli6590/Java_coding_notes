d987e4392079e634a2539cc25dc1f83faeaa54b3
abxcytf <lawrencelou88@gmail.com>
2017-04-27 12:40:41 -0400

    Create Load_Limit.java

---
+++ Week05_Lec/Load_Limit.java
@@ -0,0 +1,8 @@
+/*
+Load Limit
+
+Design a data structrue that has the ability to limit N hits in last M seconds.
+hit() if exceeds limit return false else return true;
+digest() if empty return false else return true and digest the first hit.
+*/
+
