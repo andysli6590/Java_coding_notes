e9212efb78b9913b8b95352e6acd20fb5f3c80de
abxcytf <lawrencelou88@gmail.com>
2017-05-02 10:51:31 -0400

    Create LeetCodeQ525.Contiguous_Array.java

---
+++ Week06_Lec/LeetCodeQ525.Contiguous_Array.java
@@ -0,0 +1,47 @@
+/*
+525. Contiguous Array
+Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+Example 1:
+Input: [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+Example 2:
+Input: [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+Note: The length of the given binary array will not exceed 50,000.
+*/
+
+public class Solution {
+    public int findMaxLength(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        //set 0 to -1
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] == 0 ? -1 : nums[i];
+        }
+        //key: sum, value: index
+        Map<Integer, Integer> idxMap = new HashMap<>();
+        idxMap.put(0, -1);
+        int sum = 0;
+        int max = 0;
+        //find subarray sum is equal to 0, using hashmap
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            Integer firstIdx = idxMap.get(sum);
+            if (firstIdx == null) {
+                idxMap.put(sum, i);
+            } else {
+                max = Math.max(max, i - firstIdx);
+            }
+        }
+        
+        //set the signature input array back
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] == -1 ? 0 : nums[i];
+        }
+        return max;
+    }
+}
