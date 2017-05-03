a23e1c9b44e0e0e64529ad8164efd31ef1970387
abxcytf <lawrencelou88@gmail.com>
2017-05-03 13:16:34 -0400

    Create LeetCodeQ41.First_Missing_Positive.java

---
+++ Week06/LeetCodeQ41.First_Missing_Positive.java
@@ -0,0 +1,40 @@
+/*
+41. First Missing Positive
+Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space.
+*/
+
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            //Put each number in its right place. E.g. When we find 5, then swap it with nums[4].
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        //At last, the first place where its number is not right, return the place + 1.
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        if (nums[i] != nums[j]) {
+            nums[i] ^= nums[j];
+            nums[j] ^= nums[i];
+            nums[i] ^= nums[j];
+        }
+    }
+}
