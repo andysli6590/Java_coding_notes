398e4f5dcf3faeae708f9c9dfab58b3d395b3e5b
abxcytf <lawrencelou88@gmail.com>
2017-05-07 04:08:13 -0400

    Update LeetCodeQ99.Recover_Binary_Search_Tree.java

--- Week07_Lec/LeetCodeQ99.Recover_Binary_Search_Tree.java
+++ Week07_Lec/LeetCodeQ99.Recover_Binary_Search_Tree.java
@@ -48,4 +48,57 @@ public class Solution {
         lastMin = root;
         inOrderTraverse(root.right);
     }
+    
+    /*******************************************************************************************/
+    //morris inorder traverse
+    private TreeNode lastMin = new TreeNode(Integer.MIN_VALUE); //last min value
+    private TreeNode first = null; //first elem to swap
+    private TreeNode second = null; //second elem to swap
+    public void recoverTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeNode cur = root;
+        TreeNode prev = null;
+        while (cur != null) {
+            if (cur.left == null) { //case 1: no left child -> print current
+                checkTarget(cur);
+                cur = cur.right; //go right
+            } else { //case 2: has left child
+                //step 1: find predecessor int left subtree
+                prev = cur.left;
+                while (prev.right != null && prev.right != cur) {
+                    prev = prev.right;
+                }
+                //step 2: connect predecessor and current
+                if (prev.right == null) { //case 2.1: not connected -> connect
+                    prev.right = cur;
+                    cur = cur.left;
+                } else { //case 2.2: connected -> print current and disconnect
+                    prev.right = null;
+                    checkTarget(cur);
+                    cur = cur.right;
+                }
+            }
+        }
+        System.out.println(first.val);
+        System.out.println(second.val);
+        swap(first, second);
+    }
+    
+    private void checkTarget(TreeNode cur) {
+        if (cur.val < lastMin.val) {
+            if (first == null) {
+                first = lastMin;
+            }
+            second = cur;
+        }
+        lastMin = cur;
+    }
+    
+    private void swap(TreeNode frist, TreeNode second) {
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
 }
