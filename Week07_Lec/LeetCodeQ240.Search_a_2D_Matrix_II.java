156c4133c54e77a70f0bdded42fcf98abd63005c
abxcytf <lawrencelou88@gmail.com>
2017-05-09 02:40:14 -0400

    Update LeetCodeQ240.Search_a_2D_Matrix_II.java

--- Week07_Lec/LeetCodeQ240.Search_a_2D_Matrix_II.java
+++ Week07_Lec/LeetCodeQ240.Search_a_2D_Matrix_II.java
@@ -42,4 +42,32 @@ public class Solution {
                         helper(matrix, rowMid + 1, rowEnd, colStart, colMid, target) ||
                         helper(matrix, rowMid + 1, rowEnd, colMid + 1, colEnd, target);
     }
+  
+    /*****************************************************************************************************************/
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //corner case checked
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        return binarySearch(matrix, target, 0, 0, m - 1, n - 1);
+    }
+    
+    private boolean binarySearch(int[][] matrix, int target, int startX, int startY, int endX, int endY) {
+        if (startX > endX || startY > endY) {
+            return false;
+        }
+        int midX = startX + (endX - startX) / 2;
+        int midY = startY + (endY - startY) / 2;
+        if (matrix[midX][midY] == target) { //case 1: found
+            return true;
+        } else if (matrix[midX][midY] > target) { //case 2: larger than target, go into left or up submatrix
+            return binarySearch(matrix, target, startX, startY, endX, midY - 1)
+                || binarySearch(matrix, target, startX, midY, midX - 1, endY);
+        } else { //case 3 : smaller than target, go into right or down submatrix
+            return binarySearch(matrix, target, startX, midY + 1, endX, endY) 
+                || binarySearch(matrix, target, midX + 1, startY, endX, midY);
+        }
+    }
 }
