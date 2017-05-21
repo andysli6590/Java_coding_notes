df42faff1a51e72519735357dce609c8fb4eda7f
abxcytf <lawrencelou88@gmail.com>
2017-05-20 21:57:26 -0400

    Update LeetCodeQ97.Interleaving_String.java

--- Week09_Lec/LeetCodeQ97.Interleaving_String.java
+++ Week09_Lec/LeetCodeQ97.Interleaving_String.java
@@ -35,7 +35,34 @@ public class Solution {
             return true;
         }
         
-        valid[i][j] = (i < c1.length && c1[i] == c3[k] && dfsHelper(c1, c2, c3, i + 1, j, k + 1, valid) || j < c2.length && c2[j] == c3[k] && dfsHelper(c1, c2, c3, i, j + 1, k + 1, valid))? 1 : -1;
+        valid[i][j] = (i < c1.length && c1[i] == c3[k] && dfsHelper(c1, c2, c3, i + 1, j, k + 1, valid) 
+                       || j < c2.length && c2[j] == c3[k] && dfsHelper(c1, c2, c3, i, j + 1, k + 1, valid)) ? 1 : -1;
         return valid[i][j] == 1;
+    }
+    
+    /****************************************************************************************************************************/
+    //dp implementation
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1 == null || s2 == null) {
+            return false;
+        } 
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        boolean[][] matrix = new boolean[s1.length() + 1][s2.length() + 1];
+        matrix[0][0] = true;
+        for (int i = 1; i < matrix.length; i++) {
+            matrix[i][0] = matrix[i - 1][0] && (s1.charAt(i - 1) == s3.charAt(i - 1));
+        }
+        for (int i = 1; i < matrix[0].length; i++) {
+            matrix[0][i] = matrix[0][i - 1] && (s2.charAt(i - 1) == s3.charAt(i - 1));
+        }
+        
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                matrix[i][j] = (matrix[i - 1][j] && (s1.charAt(i - 1) == s3.charAt(i + j - 1))) || (matrix[i][j - 1] && (s2.charAt(j - 1) == s3.charAt(i + j - 1)));
+            }
+        }
+        return matrix[s1.length()][s2.length()];
     }
 }
