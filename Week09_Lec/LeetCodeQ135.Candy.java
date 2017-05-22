5eee6747d67014882e49341fa44d95a052a1a0e6
abxcytf <lawrencelou88@gmail.com>
2017-05-22 15:17:45 -0400

    Update LeetCodeQ135.Candy.java

--- Week09_Lec/LeetCodeQ135.Candy.java
+++ Week09_Lec/LeetCodeQ135.Candy.java
@@ -39,4 +39,44 @@ public class Solution {
         }
         return total;
     }
+ 
+    /******************************************************************************************/
+    //optimized solution, only 1 scan from left to right
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) {
+            return 0;
+        }
+        int len = ratings.length;
+        int sum = 1;
+        int prev = 1;
+        int down = 0;
+        //one pass from left to right
+        for (int i = 1; i < len; i++) {
+            if (ratings[i] < ratings[i - 1]) {
+                down++;
+            } else {
+                //check descending sequence before
+                if (down > 0) {
+                    //step 1: add from 1 to down
+                    sum += down * (down + 1) / 2; 
+                    //1 + 2 + ...+ down, 等差数列求和
+                    //step 2: add enough on prev
+                    if (down >= prev) {
+                        sum += down - prev + 1;
+                    }
+                    prev = 1;
+                    down = 0;
+                }
+                prev = ratings[i] == ratings[i - 1] ? 1 : prev + 1;
+                sum += prev;
+            }
+        }
+        if (down > 0) {
+            sum += down * (down + 1) / 2;
+            if (down >= prev) {
+                sum += down - prev + 1;
+            }
+        }
+        return sum;
+    }
 }
