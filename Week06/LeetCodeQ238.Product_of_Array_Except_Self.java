37f287463098ba3168bf21ccf092d76b9877e5ca
abxcytf <lawrencelou88@gmail.com>
2017-05-22 02:40:34 -0400

    Update LeetCodeQ238.Product_of_Array_Except_Self.java

--- Week06/LeetCodeQ238.Product_of_Array_Except_Self.java
+++ Week06/LeetCodeQ238.Product_of_Array_Except_Self.java
@@ -18,7 +18,7 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return nums;
         }
-        int[] res = new int[nums.length];
+        int[] result = new int[nums.length];
         result[0] = 1;
         for (int i = 1; i < nums.length; i++) {
             result[i] = result[i - 1] * nums[i - 1];
