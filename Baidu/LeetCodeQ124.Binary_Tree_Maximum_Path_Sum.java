2194496a83c2f1332f92a87def2ac0b7e00d3fd6
abxcytf <lawrencelou88@gmail.com>
2017-06-07 17:33:48 -0400

    Update LeetCodeQ124.Binary_Tree_Maximum_Path_Sum.java

--- Baidu/LeetCodeQ124.Binary_Tree_Maximum_Path_Sum.java
+++ Baidu/LeetCodeQ124.Binary_Tree_Maximum_Path_Sum.java
@@ -26,7 +26,9 @@ Return 6.
  * }
  */
 public class Solution {
-    
+    //Divide and Conquer
+    //singlePath: 从root往下走到任意点的最大路径，这条路径可以不包含任何点
+    //maxPath: 从树中任意点的最大路径，这条路径至少包含一个点
     class ResultType {
         //singlePath: 从root往下走到任意点的最大路径，这条路径可以不包含任何点
         //maxPath: 从树中任意点的最大路径，这条路径至少包含一个点
@@ -57,6 +59,40 @@ public class Solution {
         
         int maxPath = Math.max(left.maxPath, right.maxPath);
         maxPath = Math.max(maxPath, left.singlePath + right.singlePath + root.val);
+        
+        return new ResultType(singlePath, maxPath);
+    }
+       
+    /**************************************************************************************************/
+    //Divide and Conquer
+    //singlePath也可以定义为只包含一个点   
+    class ResultType {
+        //singlePath也可以定义为只包含一个点
+        int singlePath;
+        int maxPath;
+        ResultType(int singlePath, int maxPath) {
+            this.singlePath = singlePath;
+            this.maxPath = maxPath;
+        }
+    }
+    
+    public int maxPathSum(TreeNode root) {
+        ResultType result = helper(root);
+        return result.maxPath;
+    }
+    
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(Integer.MIN_VALUE, Integer.MIN_VALUE);
+        }
+        //Divide
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+        
+        //Conquer
+        int singlePath = Math.max(0, Math.max(left.singlePath, right.singlePath)) + root.val;
+        int maxPath = Math.max(left.maxPath, right.maxPath);
+        maxPath = Math.max(maxPath, Math.max(left.singlePath, 0) + Math.max(right.singlePath, 0) + root.val);
         
         return new ResultType(singlePath, maxPath);
     }
